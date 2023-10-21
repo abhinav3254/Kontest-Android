3f1795fd838e81c0199a41b51754c4ef35f7262f
abhinav3254 <abhinav3254@gmail.com>
2023-10-21 13:50:45 +0530

    fix

--- app/src/main/java/com/example/kontest/database/DataBaseModel.java
+++ app/src/main/java/com/example/kontest/database/DataBaseModel.java
@@ -30,75 +30,75 @@ public class DataBaseModel {
         this.in_24_hours = in_24_hours;
         this.status = status;
     }
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getUrl() {
-//        return url;
-//    }
-//
-//    public void setUrl(String url) {
-//        this.url = url;
-//    }
-//
-//    public String getStart_time() {
-//        return start_time;
-//    }
-//
-//    public void setStart_time(String start_time) {
-//        this.start_time = start_time;
-//    }
-//
-//    public String getEnd_time() {
-//        return end_time;
-//    }
-//
-//    public void setEnd_time(String end_time) {
-//        this.end_time = end_time;
-//    }
-//
-//    public String getDuration() {
-//        return duration;
-//    }
-//
-//    public void setDuration(String duration) {
-//        this.duration = duration;
-//    }
-//
-//    public String getSite() {
-//        return site;
-//    }
-//
-//    public void setSite(String site) {
-//        this.site = site;
-//    }
-//
-//    public String getIn_24_hours() {
-//        return in_24_hours;
-//    }
-//
-//    public void setIn_24_hours(String in_24_hours) {
-//        this.in_24_hours = in_24_hours;
-//    }
-//
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getIn_24_hours() {
+        return in_24_hours;
+    }
+
+    public void setIn_24_hours(String in_24_hours) {
+        this.in_24_hours = in_24_hours;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
